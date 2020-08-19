53cc758c41d84f3030f90a3ca524206f0dd38429
Alex <lung_ali@yahoo.com>
2020-08-19 17:07:09 +0300

    refactoring

--- tema1/index.ts
+++ tema1/index.ts
@@ -1,27 +1,28 @@
 import fs, { write } from "fs";
 
 let start = new Date().getTime();
-function convertFromBaseToBase(str: number, fromBase: number, toBase: number) {
+async function convertFromBaseToBase(str: number, fromBase: number, toBase: number) {
   let num = parseInt(str.toString(), fromBase);
   return num.toString(toBase);
 }
 let convertedArray: Array<string> = [];
 let arrayNumbers: Array<string> = [];
 console.time();
-fs.readFile("./ChangeBaseNumbers.csv", "utf8", (err, file) => {
+fs.readFile("./ChangeBaseNumbers.csv", "utf8", async (err, file) => {
   let lines = file.split("\n");
-
+  let start = new Date().getTime();
   for (let line = 1; line < lines.length - 1; line++) {
     arrayNumbers = lines[line].split(",");
     convertedArray.push(
-      convertFromBaseToBase(
+      await convertFromBaseToBase(
         parseInt(arrayNumbers[0]),
         parseInt(arrayNumbers[1]),
         parseInt(arrayNumbers[2])
       )
     );
   }
-
+  let end = new Date().getTime();
+  console.log(end - start);
   let writeStream = fs.createWriteStream("OutputFile.txt");
   writeStream.write(convertedArray.toString());
 });
--- tema1/lib/index.js
+++ tema1/lib/index.js
@@ -5,19 +5,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
 let start = new Date().getTime();
-function convertFromBaseToBase(str, fromBase, toBase) {
+async function convertFromBaseToBase(str, fromBase, toBase) {
     let num = parseInt(str.toString(), fromBase);
     return num.toString(toBase);
 }
 let convertedArray = [];
 let arrayNumbers = [];
 console.time();
-fs_1.default.readFile("./ChangeBaseNumbers.csv", "utf8", (err, file) => {
+fs_1.default.readFile("./ChangeBaseNumbers.csv", "utf8", async (err, file) => {
     let lines = file.split("\n");
+    let start = new Date().getTime();
     for (let line = 1; line < lines.length - 1; line++) {
         arrayNumbers = lines[line].split(",");
-        convertedArray.push(convertFromBaseToBase(parseInt(arrayNumbers[0]), parseInt(arrayNumbers[1]), parseInt(arrayNumbers[2])));
+        convertedArray.push(await convertFromBaseToBase(parseInt(arrayNumbers[0]), parseInt(arrayNumbers[1]), parseInt(arrayNumbers[2])));
     }
+    let end = new Date().getTime();
     let writeStream = fs_1.default.createWriteStream("OutputFile.txt");
     writeStream.write(convertedArray.toString());
 });
