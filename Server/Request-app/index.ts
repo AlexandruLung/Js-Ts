229a137f8d0103901a13192f1feb8f10379d139a
Alex <lung_ali@yahoo.com>
2020-08-21 11:37:14 +0300

    Update the server and the client to be fully functional

--- Server/Request-app/index.ts
+++ Server/Request-app/index.ts
@@ -2,8 +2,10 @@ import fetch from "node-fetch";
 
 const sumURL = "http://localhost:8080/sum/2/3";
 const substractionURL = "http://localhost:8080/substract/10/5";
-const multiplicationURL = "http://localhost:8080/miltiply/10/10";
+const multiplicationURL = "http://localhost:8080/multiply/2/3";
 const divisionURL = "http://localhost:8080/divide/578/32";
+const powURL = "http://localhost:8080/pow";
+const sqrtURL = "http://localhost:8080/squareroot";
 
 fetch(sumURL)
   .then((body) => body.json())
@@ -20,3 +22,24 @@ fetch(multiplicationURL)
 fetch(divisionURL)
   .then((body) => body.json())
   .then((json) => console.log("Division: " + json));
+
+const powParams = new URLSearchParams();
+powParams.set("firstNumber", "2");
+powParams.set("secondNumber", "15");
+
+fetch(powURL, {
+  method: "POST",
+  body: powParams,
+})
+  .then((res) => res.json()) 
+  .then((json) => console.log(`Pow: ${json}`));
+
+const sqrtParams = new URLSearchParams();
+sqrtParams.set("firstNumber", "4");
+
+fetch(sqrtURL, {
+  method: "POST",
+  body: sqrtParams,
+})
+  .then((res) => res.json()) 
+  .then((json) => console.log(`Square-root: ${json}`));
--- Server/Server-app/index.ts
+++ Server/Server-app/index.ts
@@ -6,48 +6,38 @@ const port = 8080;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
 app.get("/sum/:firstParam/:secondParam", (request, response) => {
-  response.send(
-    `The result is :${
-      parseInt(request.params.firstParam) + parseInt(request.params.secondParam)
-    }`
+  response.json(
+    parseInt(request.params.firstParam) + parseInt(request.params.secondParam)
   );
 });
 app.get("/substract/:firstParam/:secondParam", (request, response) => {
-  response.send(
-    `The result is :${
-      parseInt(request.params.firstParam) - parseInt(request.params.secondParam)
-    }`
+  response.json(
+    parseInt(request.params.firstParam) - parseInt(request.params.secondParam)
   );
 });
 app.get("/divide/:firstParam/:secondParam", (request, response) => {
-  response.send(
-    `The result is :${
-      parseInt(request.params.firstParam) / parseInt(request.params.secondParam)
-    }`
+  response.json(
+    parseInt(request.params.firstParam) / parseInt(request.params.secondParam)
   );
 });
 app.get("/multiply/:firstParam/:secondParam", (request, response) => {
-  response.send(
-    `The result is :${
-      parseInt(request.params.firstParam) * parseInt(request.params.secondParam)
-    }`
+  response.json(
+    parseInt(request.params.firstParam) * parseInt(request.params.secondParam)
   );
 });
-app.get("/squareroot", (request, response) => {
-  let first=request.body.firstNumber
-  
+app.post("/squareroot", (request, response) => {
+  let first = request.body.firstNumber;
 
   let sqRoot = Math.sqrt(first);
   response.json(sqRoot);
 });
 
-app.get("/pow", (request, response) => {
-  let first=request.body.firstNumber
-  let second=request.body.secondNumber
+app.post("/pow", (request, response) => {
+  let first = request.body.firstNumber;
+  let second = request.body.secondNumber;
 
-  let pow = Math.pow(first,second);
+  let pow = Math.pow(first, second);
   response.json(pow);
 });
 
